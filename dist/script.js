57c50988aa8d7f3bed8ab21b875d653120f7df21
Lucas Araújo <lucasaraujodev.contact@gmail.com>
2023-08-09 18:10:46 -0300

    initial commit
    inital commit

---
+++ dist/script.js
@@ -0,0 +1,2 @@
+// Experimental slider with minimal JavaScript: Could and should be improved before use, not yet accessible
+// The end scroll detection is a bit flakey, so we're converting the number to a positive number (Math.abs) and giving it a ~5px leeway.
